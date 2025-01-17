3e0f1f2440a917e106670c988ddd1bd3e4da0073
KONE Madou <madou.kone@grenoble-inp.org>
2025-01-17 08:21:47 +0100

    prescan

--- src/utils.cu
+++ src/utils.cu
@@ -1,20 +1,50 @@
-#include <cmath>
 
-__global__ void coreReduce(int tab[], int len, int d){
-    int idx = threadIdx.x;
-    if(idx < len)
-        tab[idx + pow(2, d+1)] = tab[idx + pow(2, d) - 1] + tab[idx + pow(2, d) + 1 - 1] 
 
-}
+__global__ void prescan(float *g_odata, float *g_idata, int n)
+{
+  extern __shared__ float temp[];
+  // allocated on invocation
+  int thid = threadIdx.x;
+  int offset = 1;
 
-void reduce(int tab[], int len){
+  temp[2 * thid] = g_idata[2 * thid]; // load input into shared memory
+  temp[2 * thid + 1] = g_idata[2 * thid + 1];
 
-    for (size_t d = 0; d < log(len) ; d++)
+  for (int d = n >> 1; d > 0; d >> = 1)
+  // build sum in place up the tree
+  {
+    __syncthreads();
+    if (thid < d)
     {
-        coreReduce(tab, len,d)
-    }
-}
 
-void main(){
-    
+      int ai = offset * (2 * thid + 1) - 1;
+      int bi = offset * (2 * thid + 2) - 1;
+
+      temp[bi] += temp[ai];
+    }
+    offset *= 2;
+  }
+  if (thid == 0)
+  {
+    temp[n - 1] = 0;
+  } // clear the last element
+
+  for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
+  {
+    offset >> = 1;
+    __syncthreads();
+    if (thid < d)
+    {
+
+      int ai = offset * (2 * thid + 1) - 1;
+      int bi = offset * (2 * thid + 2) - 1;
+      float t = temp[ai];
+      temp[ai] = temp[bi];
+      temp[bi] += t;
+    }
+  }
+  __syncthreads();
+
+  g_odata[2 * thid] = temp[2 * thid]; // write results to device memory
+  g_odata[2 * thid + 1] = temp[2 * thid + 1];
 }
