953fc759e386e120e603977b67801125d836ea1d
Madou <madou.kone@grenoble-inp.org>
2025-01-17 10:40:34 +0100

    Prescan

--- src/utils.cu
+++ src/utils.cu
@@ -1,16 +1,15 @@
+#include <iostream>
 
-
-__global__ void prescan(float *g_odata, float *g_idata, int n)
+__global__ void prescan(int *g_idata, int n)
 {
-  extern __shared__ float temp[];
+  extern __shared__ int temp[];
   // allocated on invocation
   int thid = threadIdx.x;
   int offset = 1;
 
-  temp[2 * thid] = g_idata[2 * thid]; // load input into shared memory
-  temp[2 * thid + 1] = g_idata[2 * thid + 1];
+  temp[thid] = g_idata[thid]; // load input into shared memory
 
-  for (int d = n >> 1; d > 0; d >> = 1)
+  for (int d = n >> 1; d > 0; d = d >> 1)
   // build sum in place up the tree
   {
     __syncthreads();
@@ -31,7 +30,7 @@ __global__ void prescan(float *g_odata, float *g_idata, int n)
 
   for (int d = 1; d < n; d *= 2) // traverse down tree & build scan
   {
-    offset >> = 1;
+    offset = offset >> 1;
     __syncthreads();
     if (thid < d)
     {
@@ -45,6 +44,40 @@ __global__ void prescan(float *g_odata, float *g_idata, int n)
   }
   __syncthreads();
 
-  g_odata[2 * thid] = temp[2 * thid]; // write results to device memory
-  g_odata[2 * thid + 1] = temp[2 * thid + 1];
+
+
+  g_idata[thid] = temp[thid]; // write results to device memory
+}
+
+int main(){
+    int N = 10;
+    int tab_host[N] = {0,1,2,3,4,5,6,7,8,9};
+    int *tab_device;
+
+    const int size = N*sizeof(int);
+
+    // alloc mem on GPU
+    cudaMalloc( (void**)&tab_device, size );
+
+	cudaMemcpy(tab_device, tab_host, size, cudaMemcpyHostToDevice);
+
+    const int blocksize = 8; 
+
+    dim3 dimBlock(blocksize , 1);
+    dim3 dimGrid(2,1);
+
+    prescan<<<dimGrid,dimBlock>>>(tab_device, 10);
+
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(tab_host, tab_device, size, cudaMemcpyDeviceToHost);
+    
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout<<tab_host[i]<<" ";
+    }
+
+	std::cout<<std::endl;
+
+    
 }
